be13ec7bd8df0a3d2fc68d3827d823de41ee5f8d
alexus <afidelo@protonmail.com>
2019-08-15 01:04:01 +0200

    commercial flows added ( import only )

--- app/app.js
+++ app/app.js
@@ -10,10 +10,7 @@ app.use(bodyParser.json());
 
 
 const {country} = require('./models');
-const {consumption} = require('./models');
-const {auctionDaily} =  require('./models');
-const {loadrealized} = require('./models')
-const {loadforecast} = require('./models')
+const {commercialflows} = require('./models')
 
 
 require('./routes/api/v1/')(app);
@@ -23,7 +20,7 @@ app.get('/', async (req, res) =>
   // this should be function to extract from excel, prepare results in array, and then post to `auction/daily/create`
   // uzeti auction manual kao referentnu tačku; ( testirati na `auction-daily-sample-to-become`);
 
-  var filename = "/src/utils/excel-parser-scripts/load-realized/load-auto.xlsx";
+  var filename = "/src/utils/excel-parser-scripts/commercial-flows/com-flow-auto.xlsx";
 
   const XLXS = require('xlsx');
 
@@ -31,10 +28,8 @@ app.get('/', async (req, res) =>
 
   const sheetNameList = workbook.SheetNames;
 
-  let resultingInsert = null;
-
-  let countryId = null;
-  
+  let firstCountryId = null;
+  let secondCountryId = null;
 
   // {header: 1} -> returns header as first array, results as anothers; 
   let toMap = XLXS.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]], {header: 1});
@@ -45,27 +40,42 @@ app.get('/', async (req, res) =>
 
   let final;
 
-  let headers = toMap[0].map(header => {
-
-    // console.log('header: ', header)
-    return header;
-  })
+  let headers = toMap[0].map(header => header)
   let timestamp = headers[0]
   headers.splice(0,1)
  
   for(let i = 0; i<headers.length; i++){
-    countryId = await country.findOne({where:{code:headers[i]}})
-    for(let j = 0; j < result.length; j++){
-      finalArray.push({timestamp: result[i][timestamp] , countryId: countryId.id, value: result[i][headers[i]], automaticallyUpdated:0, createdAt: Date.now(), updatedAt: Date.now() })
+
+    try {
+
+      firstCountryId = await country.findOne({where:{code:headers[i].substring(0, 2)}})
+      secondCountryId = await country.findOne({where:{code:headers[i].substring(2, 4)}})
+      // console.log('first country id ( from database ): ', firstCountryId)
+      // console.log('second country id ( from database ): ', secondCountryId)
+      for(let j = 0; j < result.length; j++){
+        finalArray.push(
+          {firstCountryId: firstCountryId.id === null? 71: firstCountryId.id, secondCountryId: secondCountryId.id === null? 71: secondCountryId.id, code: headers[i].substring(0, 2) + headers[i].substring(2, 4), displayCode: firstCountryId.displayCode + '-' + secondCountryId.displayCode, timestamp: result[i][timestamp], value: result[i][headers[i]], automaticallyUpdated:0, createdAt: Date.now(), updatedAt: Date.now() })
+      }
+      
+    } catch (error) {
+      console.log('error: ', error)
     }
+
+    // for(let j = 0; j < result.length; j++){
+    //   finalArray.push(
+    //     {firstCountryId: firstCountryId.id === null? 71: firstCountryId.id, secondCountryId: secondCountryId.id === null? 71: secondCountryId.id, code: headers[i].substring(0, 2) + headers[i].substring(2, 4), displayCode: firstCountryId.displayCode + '-' + secondCountryId.displayCode, timestamp: result[i][timestamp], value: result[i][headers[i]], automaticallyUpdated:0, createdAt: Date.now(), updatedAt: Date.now() })
+    // }
   }
   try
   {
-    //84241
-    const firstBulk = finalArray.splice(0, finalArray.length/2)
-    const secondBulk = finalArray.splice(0, finalArray.length)
-    let test = await loadforecast.bulkCreate(firstBulk)
-    let anothertest = await loadforecast.bulkCreate(secondBulk)
+    // console.log('length of final array: ', finalArray.length)
+    const firstBulk = finalArray.splice(0, finalArray.length/3)
+    // const secondBulk = finalArray.splice(0, finalArray.length/2)
+    // const thirdBulk = finalArray.splice(0, finalArray.length)
+    let test = await commercialflows.bulkCreate(firstBulk)
+    // let anothertest = await commercialflows.bulkCreate(secondBulk)
+    // let anothertest1 = await commercialflows.bulkCreate(thirdBulk)
+
     res.status(200).json('success');
 
   }
---
+++ app/controllers/commercial-flows-controller.js
@@ -0,0 +1,28 @@
+const {commercialflows} = require('../models');
+
+const moment = require('moment');
+
+module.exports = 
+{
+    
+    async list(req, res)
+    {
+        let result = null;
+        
+        try
+        {
+            result = await loadrealized.commercialflows({where:{countryId:36}});
+            
+        }
+        catch (error)
+        {
+
+            return res.status(400).json(error);
+
+        }
+        
+        return res.status(200).json(result);
+
+    }
+
+}
